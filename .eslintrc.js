5e546a2173caa9fbc50d3924ba6eca475e80ecf6
Nikolas Laus <bchik3@gmail.com>
2021-11-30 09:42:10 +0200

    Change ESLint config to @vue/recommended.

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,14 +4,15 @@ module.exports = {
     node: true
   },
   extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
+    'plugin:vue/recommended',
+    'eslint:recommended'
   ],
   parserOptions: {
     parser: 'babel-eslint'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'semi': [1, 'always']
   }
 }
