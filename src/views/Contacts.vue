b787faf0f6ff47263a6f70b207fb3dbd5e8ef977
Roasteg <krostislav468@gmail.com>
2021-12-01 06:13:40 +0200

    Fix contact editing

--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -186,6 +186,7 @@ export default {
         phone: this.editContactCredentials.phone,
         email: this.editContactCredentials.email,
       }).then(() => {
+        this.getContacts();
         this.editButtonLoading = false;
         this.editDialog = false;
       }).catch(() => {
